8f039dde17aa447c9381b7ae6f52d3a9abcf963b
Jesse Richardson <54087199+structuralcoder@users.noreply.github.com>
2019-08-15 10:51:17 -0500

    Update network.py
    update 1.2

--- network.py
+++ network.py
@@ -1,31 +1,55 @@
-import socket
-import pickle
+import pygame
+
+def custom_vertices(x,y,z,w=.2,h=.5,d=.05):
+	return [
+		x-w,y+h,z-d, x-w,y+h,z+d, x+w,y+h,z+d, x+w,y+h,z-d,  # top
+		x-w,y-h,z-d, x+w,y-h,z-d, x+w,y-h,z+d, x-w,y-h,z+d,  # bottom
+		x-w,y-h,z-d, x-w,y-h,z+d, x-w,y+h,z+d, x-w,y+h,z-d,  # left
+		x+w,y-h,z+d, x+w,y-h,z-d, x+w,y+h,z-d, x+w,y+h,z+d,  # right
+		x-w,y-h,z+d, x+w,y-h,z+d, x+w,y+h,z+d, x-w,y+h,z+d,  # front
+		x+w,y-h,z-d, x-w,y-h,z-d, x-w,y+h,z-d, x+w,y+h,z-d,  # back
+	]
+
+def tex_coord(x, y, n=4):
+	""" Return the bounding vertices of the texture square.
+	"""
+	m = 1.0 / n
+	dx = x * m
+	dy = y * m
+	return dx, dy, dx + m, dy, dx + m, dy + m, dx, dy + m
 
 
-class Network:
-	def __init__(self,server="10.0.0.59",port=5555):
-		self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server = server
-		self.port = port
-		self.addr = (self.server, self.port)
-		self.p = self.connect()
+def tex_coords(top, bottom, side):
+	""" Return a list of the texture squares for the top, bottom and side.
+	"""
+	top = tex_coord(*top)
+	bottom = tex_coord(*bottom)
+	side = tex_coord(*side)
+	result = []
+	result.extend(top)
+	result.extend(bottom)
+	result.extend(side * 4)
+	return result
 
-	def getP(self):
-		return self.p
+BLACK = tex_coords((3,1), (3,1), (3,1))
+SILVER = tex_coords((1,2),(1,2),(1,2))
+WOOD_PLANKS = tex_coords((3,0), (3,0), (3,0))
+VADER_FACE = tex_coords((0,2), (0,2), (0,2))
+BULLET=tex_coords((3,1), (3,1), (3,1))
 
-	def connect(self):
-		try:
-			self.client.connect(self.addr)
-			return self.client.recv(2048).decode()
-		except:
-			pass
+
+
+class pistol:
+	def __init__(self):
+		self.auto=False
+		self.damage=30
+		#color then vertices
+		self.blocks=[SILVER,custom_vertices(0,0.13,-.30,.05,.05,.18),
+			SILVER,custom_vertices(0,0.05,-.15,.04,.1,.02)
+		]
+		self.sound=pygame.mixer.Sound('sounds/380_gunshot.wav')
 	
-	
-	
-	def send(self, data):
-		try:
-			self.client.send(str.encode(data))
-			return pickle.loads(self.client.recv(2048*4))
-		except socket.error as e:
-			print(e)
-
+		
+		
+		
+		
